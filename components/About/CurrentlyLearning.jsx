8bfad81ca574062051d098e677baa030c96478c7
Dudley Spence <dudleyspence5@gmail.com>
2025-02-09 10:47:59 +0100

    icon update

--- components/About/CurrentlyLearning.jsx
+++ components/About/CurrentlyLearning.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import SkillCard from "./SkillCard";
+import nextIcon from "@/public/Skills/nextjs.png";
+import typescriptIcon from "@/public/Skills/typescript.png";
 
 export default function CurrentlyLearning({ marquee }) {
   const skill2 = (
-    <SkillCard
-      imagePath="@/public/Skills/typescript.png"
-      name="TypeScript"
-      marquee={marquee}
-    />
+    <SkillCard imagePath={typescriptIcon} name="TypeScript" marquee={marquee} />
   );
   const skill1 = (
-    <SkillCard
-      imagePath="@/public/Skills/nextjs.png"
-      name="Next.js"
-      marquee={marquee}
-    />
+    <SkillCard imagePath={nextIcon} name="Next.js" marquee={marquee} />
   );
   return (
     <div className="w-full flex flex-row gap-5 mt-3">
